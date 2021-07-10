907f8bad039abe2755ecc326e9ceaa9d0c838184
jainharshit27 <82932932+jainharshit27@users.noreply.github.com>
2021-07-10 11:04:20 +0530

    Add files via upload

---
+++ C7 AA2 reference.py
@@ -0,0 +1,15 @@
+# WHILE LOOP
+import random
+ring = random.randint(1,18)
+print(ring)
+
+i = 1
+while i<=18:
+    if i == ring:
+        print(i)
+        break
+    else:
+        print(i)
+        i+=1
+        
+     
